78ca80db7b64b4d9b475597c8af7fa59f299bd97
daytrick <120808108+daytrick@users.noreply.github.com>
2025-02-22 15:57:48 +0000

    Fixed CSS/appearances

--- content/content.js
+++ content/content.js
@@ -2,7 +2,7 @@ const aiDIV = document.querySelector("#Odp5De > div:nth-child(1)");
 var yeetStatus = undefined;
 
 chrome.runtime.onConnect.addListener((port) => {
-   port.onMessage.addListener((msg) => {
+    port.onMessage.addListener((msg) => {
         if (msg.from == "AI Yeeter") {
             port.postMessage({to: "AI Yeeter", status: yeetStatus});
             console.log(`Posted message ${yeetStatus}`);
@@ -13,9 +13,7 @@ chrome.runtime.onConnect.addListener((port) => {
 if (aiDIV) {
     aiDIV.remove();
     yeetStatus = true;
-    console.log("Yeeted");
 }
 else {
     yeetStatus = false;
-    console.log("Nothing to yeet");
 }
